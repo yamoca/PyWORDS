b9009482ba1ac526073bc9a32705f2d90a212af5
leo <ljgulliver256@gmail.com>
2024-05-30 17:08:30 +0000

    added column headings to reduced inflects.tsv

--- pywords/data/db.py
+++ pywords/data/db.py
@@ -2,43 +2,43 @@ import sqlite3
 con = sqlite3.connect("minimal.db") # connection
 cur = con.cursor() # cursor
 
-column_types = {
-    'dl_id': 'INTEGER',
-    'dl_stem1': 'TEXT',
-    'dl_stem2': 'TEXT',
-    'dl_stem3': 'TEXT',
-    'dl_stem4': 'TEXT',
-    'dl_pos': 'TEXT',
-    'dl_type': 'INTEGER',
-    'dl_variant': 'INTEGER',
-    'dl_gender': 'TEXT',
-    'dl_comparison': 'INTEGER',
-    'dl_kind': 'TEXT',
-    'dl_aux_case': 'TEXT',
-    'dl_age': 'TEXT',
-    'dl_area': 'TEXT',
-    'dl_geography': 'TEXT',
-    'dl_frequency': 'TEXT',
-    'dl_source': 'TEXT',
-    'dl_senses': 'TEXT'
-}
+def create_dictionary_db():
+    column_types = {
+        'dl_id': 'INTEGER',
+        'dl_stem1': 'TEXT',
+        'dl_stem2': 'TEXT',
+        'dl_stem3': 'TEXT',
+        'dl_stem4': 'TEXT',
+        'dl_pos': 'TEXT',
+        'dl_type': 'INTEGER',
+        'dl_variant': 'INTEGER',
+        'dl_gender': 'TEXT',
+        'dl_comparison': 'INTEGER',
+        'dl_kind': 'TEXT',
+        'dl_aux_case': 'TEXT',
+        'dl_age': 'TEXT',
+        'dl_area': 'TEXT',
+        'dl_geography': 'TEXT',
+        'dl_frequency': 'TEXT',
+        'dl_source': 'TEXT',
+        'dl_senses': 'TEXT'
+    }
 
 
-dictionary = open("DICTLINE.TSV", 'r')
-lines = dictionary.readlines()
+    dictionary = open("DICTLINE.TSV", 'r')
+    lines = dictionary.readlines()
 
 
-column_names = list(column_types.keys())
-columns_str = ', '.join([f"{column} {column_types.get(column)}" for column in column_names])
-create_table_query = f"CREATE TABLE IF NOT EXISTS words ({columns_str})"
-cur.execute(create_table_query)
+    column_names = list(column_types.keys())
+    columns_str = ', '.join([f"{column} {column_types.get(column)}" for column in column_names])
+    create_table_query = f"CREATE TABLE IF NOT EXISTS words ({columns_str})"
+    cur.execute(create_table_query)
 
-# must construct this as dont want to write out 17 lots of (?, ?, ?, ) by hand
-insert_query = "INSERT INTO words VALUES (" + ', '.join(['?' for _ in column_names]) + ")"
-for line in lines[1:]:
-    values = line.split() 
-    print(insert_query, values)
-    cur.execute(insert_query, values)
+    # must construct this as dont want to write out 17 lots of (?, ?, ?, ) by hand
+    insert_query = "INSERT INTO words VALUES (" + ', '.join(['?' for _ in column_names]) + ")"
+    for line in lines[1:]:
+        values = line.split() 
+        print(insert_query, values)
+        cur.execute(insert_query, values)
 
-
-con.commit()
+    con.commit()
